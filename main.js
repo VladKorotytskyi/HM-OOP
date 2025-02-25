5dcaf6f2db68a4533401b72c7353e194d165838a
DizzyYT <153006021+VladKorotytskyi@users.noreply.github.com>
2025-02-25 17:21:20 +0100

    ///

--- main.js
+++ main.js
@@ -29,6 +29,29 @@
 // poly.getInfo(); // User Poly is 3 years old and has 17 followers
 
 //3
+//Напиши клас Storage, який буде створювати об'єкти для управління складом товарів. При виклику буде отримувати один аргумент - початковий масив товарів, і записувати його у властивість items.
+//Додай методи класу:
+//getItems() — повертає масив поточних товарів
+//addItem(item) — отримує новий товар і додає його до поточних
+//removeItem(item) — отримує товар і, якщо він є, видаляє його з поточних
+class Storage {
+    constructor(items) {
+      this.items = items;
+    }
+  
+    getItems() {
+      return this.items;
+    }
+  
+    addItem(item) {
+      this.items.push(item);
+    }
+  
+    removeItem(item) {
+      this.items = this.items.filter(existingItem => existingItem !== item);
+    }
+  }
+
 const storage = new Storage([
     'Нанітоіди',
     'Пролонгер',
